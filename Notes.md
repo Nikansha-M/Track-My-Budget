64df07a4920a1e6f69e5b663115c223b6942a766
Nikansha Maharaj <nikansha.m@gmail.com>
2023-03-28 17:49:13 -0700

    initial commit

---
+++ Notes.md
@@ -0,0 +1,6 @@
+### Getting Started
+1. Use create-react-app to initialize project
+1. Open VS Code
+1. Run npm install to install package dependencies from package.json
+1. Run npm start to start development server on localhost: 3000
+1. CTRL + C to exit development server
--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: 'a1',
+      title: 'Internet',
+      amount: 94.12,
+      date: new Date(2023, 0, 14),
+    },
+    {
+      id: 'a2',
+      title: 'New TV',
+      amount: 1799.49,
+      date: new Date(2023, 0, 12)
+    },
+    {
+      id: 'a3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2023, 1, 28),
+    },
+    {
+      id: 'a4',
+      title: 'New Desk',
+      amount: 476.36,
+      date: new Date(2023, 2, 12),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Let's Make A Budget 🪙</h1>
+      <Expenses items={expenses} />
     </div>
   );
 }
